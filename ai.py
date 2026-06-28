3a9faa3aca8945c106a7d994550163380e286fcc
Hou-Tianqi <2702200569@qq.com>
2026-06-28 10:24:37 +0800

    更新README，添加使用说明；修改ai.py以支持不同条件下的上下文长度和模型选择

--- ai.py
+++ ai.py
@@ -6,26 +6,33 @@ global ctext,tmpt,top_p,max_tokens,condition,llm
 
 model = ["./models/Qwen3-1.7B-Q4_K_M.gguf","./models/Qwen3.5-0.8B-Q4_K_M.gguf"]
 
-llm = Llama(
-    model_path=model[1],
-    n_ctx=4096,
-    n_threads=4,
-    n_gpu_layers=0,
-    use_mlock=True,
-    verbose=False,
-    last_n_tokens_size=64
-)
+
 print("模型加载完成！")
 print("模型回答中的<think>…………</think>是深度思考的思维链")
 
 ctext = [{"role": "system", "content": "你是一个AI助手，回答要简洁，避免废话。"}]
-
-temperature = {"默认":{"max_tokens":512,"top_p":0.85,"tmpt":0.4},"日常":{"max_tokens":256,"top_p":0.75,"tmpt":0.4},"写作":{"max_tokens":1200,"top_p":0.8,"tmpt":0.5},"代码":{"max_tokens":1000,"top_p":0.9,"tmpt":0.2}}
-
+temperature = {"默认":{"max_tokens":512,"top_p":0.85,"tmpt":0.4},"日常":{"max_tokens":256,"top_p":0.75,"tmpt":0.4},"写作":{"max_tokens":1500,"top_p":0.8,"tmpt":0.5},"代码":{"max_tokens":None,"top_p":0.9,"tmpt":0.2}}
 condition = "代码"
 max_tokens = temperature[condition]["max_tokens"]
 tmpt = temperature[condition]["tmpt"]
 top_p = temperature[condition]["top_p"]
+n_ctx = 4096
+if condition == "日常":
+    ctext[0]["content"] += "/no_think"
+if condition == "代码":
+    n_ctx = 8192
+if condition == "写作":
+    n_ctx = 6000
+
+llm = Llama(
+    model_path=model[0], #这里是你选择model列表的第几项，一般来说填0就行
+    n_ctx=n_ctx,
+    n_threads=4, #设置为你的CPU物理核心数，注意，是物理核心数，比如我的i5 11320H是4核心8线程，所以设为4
+    n_gpu_layers=0, #如果有英伟达的显卡的话可以调高，用GPU加速
+    use_mlock=True,
+    verbose=False,
+    last_n_tokens_size=64,
+)
 
 def generate_response(msg, max_tokens=512, temperature=0.4, top_p=0.8):
     start = None
@@ -76,7 +83,7 @@ def main():
             tool.MD_to_HTML()
             continue
         if pmt == "LaTeX":
-            LaTex = input()
+            LaTex = input("LaTeX:")
             tool.render_from_text(LaTex,"./")
             continue
         talk(pmt)
